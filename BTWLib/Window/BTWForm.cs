734aebbc0f77efcbab3b904c2bb7d10f900ed49b
Serg Chernoivanenko <LonelyKeter@gmail.com>
2019-06-10 09:53:40 +0300

    Started

---
+++ BTWLib/Window/BTWForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace BTWLib
+{
+	public partial class BTWForm : Form
+	{
+		[DllImport("user32.dll")]
+		public static extern int SendMessage(IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam);
+
+		private const int WM_SETREDRAW = 11;
+
+		public void SuspendDrawing()
+		{
+			SendMessage(this.Handle, WM_SETREDRAW, false, 0);
+		}
+
+		public void ResumeDrawing()
+		{
+			SendMessage(this.Handle, WM_SETREDRAW, true, 0);
+			this.Refresh();
+		}
+
+
+		public BTWForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Test/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BTWLib;
+using Keter.Core;
+
+namespace Test
+{
+	static class Test
+	{
+		/// <summary>
+		/// Главная точка входа для приложения.
+		/// </summary>
+		static void Main()
+		{
+			BTWLogic LOGIC = new BTWLogic();
+			BTWIO IO = new BTWIO();
+
+			LOGIC.IO = IO;
+			IO.LOGIC = LOGIC;
+
+			IO.Init();
+			LOGIC.Init();		
+
+			KTApplication App = new KTApplication(LOGIC, IO, 20);
+
+			App.Init();
+
+			App.Run();
+		}
+	}
+}
